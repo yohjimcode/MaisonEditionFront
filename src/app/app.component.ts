58de34088e2018e5cd9f811c3c311927d19c352d
yohjimcode <collignon.mickael1987@gmail.com>
2024-10-21 23:34:34 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './pages/header/header.component';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { HeaderComponent } from './pages/header/header.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
